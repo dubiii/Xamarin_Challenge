17b0d93467ed570b8e401210b9ac263ef7197eef
Mario Piñones <dubi@DESKTOP-5838I18>
2017-04-18 11:59:11 -0300

    Proyecto Xamarin Challenge Finalizado

---
+++ CoolBreeze/CoolBreeze/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoolBreeze.ViewModels;
+using Xamarin.Forms;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
+namespace CoolBreeze
+{
+    public partial class App : Application
+    {
+        public static MainViewModel ViewModel;
+        public static string RegistrationCode = "XBWWD9F4C1FE1799F627DDA93C4440CE";
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new StartPage());
+        }
+
+        protected override void OnStart()
+        {
+            MobileCenter.Start(typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ CoolBreeze/CoolBreeze/StartPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoolBreeze.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CoolBreeze
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage : TabbedPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            InitializeAppAsync();
+            base.OnAppearing();
+        }
+
+        private void InitializeAppAsync()
+        {
+            //DateTime badDateTime = DateTime.Parse("This is not a date or time");
+
+            if (App.ViewModel == null) App.ViewModel = new MainViewModel();
+        }
+
+        private async void ViewForecastClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ForecastPage());
+        }
+
+        private async void ViewSubmitChallengeClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SubmissionPage());
+        }
+    }
+}
